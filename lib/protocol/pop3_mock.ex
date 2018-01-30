333a7cb2e1365150dcdd52617540a80be1f97745
Yaroslav Zemlyanuhin <d4rk5eed@yandex.ru>
2018-01-30 17:36:04 +0300

    - Created Email.Protocol.Pop3 for pop3 protocol - Created Email.Protocol.Pop3.Mock for testing

---
+++ lib/protocol/pop3_mock.ex
@@ -0,0 +1,35 @@
+defmodule Agala.Provider.Email.Protocol.Pop3.Mock do
+  @moduledoc """
+  Implementation of mock module for Pop3 mail protocol of Agala framework.
+  For testing purposes
+  """
+  alias Agala.Provider.Email.Protocol.Pop3
+
+  def connect(%{email: _, password: _, server: _, port: _, login: _}) do
+    {:ok,
+     {:sk, 'user', 'mail.example.com',
+      {:sslsocket, {:gen_tcp, "some_port", :tls_connection, :undefined},
+       self()}, 995, false, false, true}}
+  end
+
+  def scan(_client) do
+    {:ok, [{1, 1948}, {2, 1923}, {3, 1123}]}
+  end
+
+  def retrieve(_client, id) do
+    {:ok,
+      "To: <receiver@example.com@yandex.ru>\r\nFrom: Sender <sender@example.com>\r\nSubject: =?UTF-8?B?0LLQvtC/0YDQvtGBINC/0L4g0LrRgNC10LTQuNGC0YM=?=\r\nDate: Tue, 30 Jan 2018 16:22:53 +0300\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101\r\n Thunderbird/52.5.2\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=\"utf-8\"; format=flowed\r\nContent-Transfer-Encoding: 8bit\r\nContent-Language: en-US\r\n\r\nmessage number #{id}\r\n\r\n"}
+  end
+
+  def parse_binary(raw_binary) do
+    Pop3.parse_binary(raw_binary)
+  end
+
+  def parse_headers({:message, headers, content}) do
+    Pop3.parse_headers({:message, headers, content})
+  end
+
+  def parse_content({:message, headers, content}) do
+    Pop3.parse_content({:message, headers, content})
+  end
+end
--- mix.exs
+++ mix.exs
@@ -25,7 +25,9 @@ defmodule AgalaEmail.MixProject do
     [
       {:agala, "~> 2.0.2"},
       {:ex_doc, "> 0.0.0", only: :dev},
-      {:credo, "~> 0.8", only: [:dev, :test]}
+      {:credo, "~> 0.8", only: [:dev, :test]},
+      {:pop3mail, "~> 1.3"},
+      {:erlpop, github: "nico-amsterdam/erlpop"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
