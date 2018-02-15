c4f986fd661f139a5d4166aa6289a75e5a738b8b
Victoria <v.bykova@cti.ru>
2018-02-15 17:16:36 +0300

    add Agala.Provider.Email.Chain.Parser

---
+++ lib/models/response.ex
@@ -0,0 +1,7 @@
+defmodule Agala.Provider.Email.Model.Response do
+  defstruct name: nil,
+            email: nil,
+            message_id: nil,
+            subject: nil,
+            message: []
+end
