43332471bc782644acaab5fdbf193f2979cf98b9
Victoria <v.bykova@cti.ru>
2018-02-18 00:45:53 +0300

    email helpers

---
+++ lib/helpers.ex
@@ -0,0 +1,12 @@
+defmodule Agala.Provider.Telegram.Helpers do
+  import Bamboo.Email
+
+  def send_message(to, from, subject, message, mailer) do
+    new_email
+    |> to(to)
+    |> from(from)
+    |> subject(subject)
+    |> html_body(message)
+    |> mailer.deliver_now
+  end
+end
