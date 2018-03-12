93f8865996701b64079561b492983dc10e3b5cd2
Victoria <v.bykova@cti.ru>
2018-03-12 17:17:09 +0300

    remove mail sending form agala_email

--- lib/helpers.ex
+++ lib/helpers.ex
@@ -1,14 +1,10 @@
 defmodule Agala.Provider.Email.Helpers do
   import Bamboo.Email
 
-  def send_message(to, from, subject, message, reply_message_id, mailer) do
+  def generate_message(to, from, subject) do
     new_email
     |> to(to)
     |> from(from)
     |> subject(subject)
-    |> html_body(message)
-    |> put_header("In-Reply-To", reply_message_id)
-    |> put_header("References", reply_message_id)
-    |> mailer.deliver_now
   end
 end
